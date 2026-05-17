4c81e1d1f3ecde221a0a05f48c2e51dc89d68b85
Jeremy Trimble <jeremy.trimble@gmail.com>
2026-05-17 08:10:37 -0400

    test_chunkwise_pool.py: Don't directly close the Pool, as this messes up ProcessingPoolManager's internal state and causes subsequent tests to fail.

--- tests/test_chunkwise_pool.py
+++ tests/test_chunkwise_pool.py
@@ -21,5 +21,30 @@ def test_chunkwise_pool_basics():
     pool.map_async(add5, (10,11,12,13,100), callback=handle_result_cb, error_callback=lambda e: print(f"Error in pool: {e}")).get()
 
     assert results == [[15,16,17,18,105]]
-    pool.close()
+    del pool
+    ppm.close() # Call close() on the ProcessingPoolManager, not on the pool itself, since the pool is managed by the manager
+
+def test_chunkwise_pool_can_restart():
+    ppm = ProcessingPoolManager.get_instance()
+    assert ppm is not None
+
+    pool = ppm.get_pool()
+
+    results = []
+    def handle_result_cb(res):
+        print(f"Got result: {res}")
+        results.append(res)
+
+    pool.map_async(add5, (10,11,12,13,100), callback=handle_result_cb, error_callback=lambda e: print(f"Error in pool: {e}")).get()
+    assert results == [[15,16,17,18,105]]
+    del pool
+    ppm.close()
+
+    # Restart the pool and run another job
+    pool = ppm.get_pool()
+    results.clear()
+    pool.map_async(add5, (20,21), callback=handle_result_cb, error_callback=lambda e: print(f"Error in pool: {e}")).get()
+    del pool
+    assert results == [[25,26]]
+    ppm.close()
 
